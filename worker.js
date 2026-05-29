0050334e553546a147b413b7da066e6505091a32
Fernando Jara <jarafer96@gmail.com>
2026-05-29 15:09:15 -0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -29,7 +29,7 @@ export default {
     const objectKey = `productos/${email_vendedor}/${Date.now()}-${fileName}`;
 
     // Endpoint nativo de R2
-    const endpoint = 'https://img.mpagina.com.ar';
+    const endpoint = 'https://mpage-db.a2f89bcf2254aa9ff406c31073099c0c.r2.cloudflarestorage.com';
 
     // Generar la URL prefirmada
     const presignedUrl = await generatePresignedUrl(
